4c6001e2ab5365c8a53f9f204cd502a5a6f428ed
mrmikeyc <michaelchan2018@gmail.com>
2021-03-11 16:28:13 -0500

    Created initial create post activity and menu bar navigation to activity

--- app/src/main/java/com/example/parsegram/FeedActivity.java
+++ app/src/main/java/com/example/parsegram/FeedActivity.java
@@ -1,14 +1,42 @@
 package com.example.parsegram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private static final int CREATE_POST_REQUEST_CODE = 200;
+    private static final String TAG = FeedActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.menuBtnCreatePost) {
+            // Go to create post activity
+            Log.i(TAG, "Starting create activity!");
+            Intent intent = new Intent(this, CreatePostActivity.class);
+            startActivityForResult(intent, CREATE_POST_REQUEST_CODE);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
