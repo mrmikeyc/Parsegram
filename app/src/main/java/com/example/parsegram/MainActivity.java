56b087f074dfb81de79f0d153c78d650f8c3a844
mrmikeyc <michaelchan2018@gmail.com>
2021-03-11 13:23:05 -0500

    Initial login activity generated; Initial intro menu created

--- app/src/main/java/com/example/parsegram/MainActivity.java
+++ app/src/main/java/com/example/parsegram/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.parsegram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.parsegram.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LOGIN_ACTIVITY_REQUEST_CODE = 100;
+
+    Button btnLogin;
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start intent to go to login activity
+                goToLoginActivity();
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoSignupActivity();
+            }
+        });
+    }
+
+    private void gotoSignupActivity() {
+    }
+
+    private void goToLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivityForResult(intent, LOGIN_ACTIVITY_REQUEST_CODE);
     }
 }
