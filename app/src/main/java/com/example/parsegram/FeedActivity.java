1c96fd4860fdad6e52554615eb069de073c2860b
mrmikeyc <michaelchan2018@gmail.com>
2021-03-21 21:52:47 -0400

    Integrated create post fragment into the main navigation fragment layout; created initial fragment to contain feed

--- app/src/main/java/com/example/parsegram/FeedActivity.java
+++ app/src/main/java/com/example/parsegram/FeedActivity.java
@@ -5,16 +5,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.parsegram.fragments.ComposeFragment;
+import com.example.parsegram.fragments.FeedFragment;
 import com.example.parsegram.models.FeedPost;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
@@ -23,110 +27,141 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+// TODO: Rename FeedActivity to be more generic because now it has rotating fragments
 public class FeedActivity extends AppCompatActivity {
 
     private static final int CREATE_POST_REQUEST_CODE = 200;
     private static final int USER_SETTINGS_REQUEST_CODE = 300;
     private static final String TAG = FeedActivity.class.getSimpleName();
 
-    FloatingActionButton fabCreatePost;
-    List<FeedPost> posts;
-    PostAdapter adapter;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+//    List<FeedPost> posts;
+//    PostAdapter adapter;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        fabCreatePost = findViewById(R.id.fabCreatePost);
-
-        fabCreatePost.setOnClickListener(new View.OnClickListener() {
+        // Navigation bar initialization
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                goToCreateActivity();
-            }
-        });
-
-        RecyclerView rvPosts = (RecyclerView) findViewById(R.id.rvPosts);
-        posts = new ArrayList<>();
-        adapter = new PostAdapter(this, posts);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        rvPosts.setLayoutManager(linearLayoutManager);
-        rvPosts.setAdapter(adapter);
-
-        fetchPosts();
-    }
-
-    private void fetchPosts() {
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-
-        // Include a specific key so that we can narrow down our posts (i.e. by user)
-        // query.include(Post.KEY_USER);
-
-        // Launch actual post
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> queriedPosts, ParseException e) {
-                // The list of all post objects that fit our query
-
-                if (e == null) {
-                    try {
-                        // Note that a Post is a db response item, and a FeedPost is a model
-                        posts.addAll(FeedPost.createFeedPostList(queriedPosts));
-                        Collections.reverse(posts);
-                    } catch (ParseException parseException) {
-                        parseException.printStackTrace();
-                    }
-                    adapter.notifyDataSetChanged();
-                } else {
-                    Log.e(TAG, "Error querying post data: " + e);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // MenuItem is one of the item id's defined in the nav bar xml file
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        // TODO: Update fragment to go to feed
+                        Toast.makeText(FeedActivity.this, "Navigating Home", Toast.LENGTH_SHORT).show();
+                        fragment = new FeedFragment();
+                        break;
+                    case R.id.action_compose:
+                        Toast.makeText(FeedActivity.this, "Navigating Create", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_profile:
+                        // TODO: Update fragment to go to profile
+                        Toast.makeText(FeedActivity.this, "Navigating Profile", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
+                        break;
+                    default:
+                        // TODO: Find way to eliminate default case - or make it a reasonable default
+                        fragment = new ComposeFragment();
                 }
+
+                // Whatever fragment we have reassigned to
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
         });
+        // Default navigation selection (good for when the app starts up)
+        // TODO: Change this to Feed Fragment
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+
+        // TODO: Move RV to a fragment
+//        RecyclerView rvPosts = (RecyclerView) findViewById(R.id.rvPosts);
+//        posts = new ArrayList<>();
+//        adapter = new PostAdapter(this, posts);
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        rvPosts.setLayoutManager(linearLayoutManager);
+//        rvPosts.setAdapter(adapter);
+//
+//        fetchPosts();
     }
 
-    private void goToCreateActivity() {
-        Log.i(TAG, "Starting create activity");
-        Intent intent = new Intent(FeedActivity.this, CreatePostActivity.class);
-        startActivityForResult(intent, CREATE_POST_REQUEST_CODE);
-    }
+//    private void fetchPosts() {
+//        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+//
+//        // Include a specific key so that we can narrow down our posts (i.e. by user)
+//        // query.include(Post.KEY_USER);
+//
+//        // Launch actual post
+//        query.findInBackground(new FindCallback<Post>() {
+//            @Override
+//            public void done(List<Post> queriedPosts, ParseException e) {
+//                // The list of all post objects that fit our query
+//
+//                if (e == null) {
+//                    try {
+//                        // Note that a Post is a db response item, and a FeedPost is a model
+//                        posts.addAll(FeedPost.createFeedPostList(queriedPosts));
+//                        Collections.reverse(posts);
+//                    } catch (ParseException parseException) {
+//                        parseException.printStackTrace();
+//                    }
+//                    adapter.notifyDataSetChanged();
+//                } else {
+//                    Log.e(TAG, "Error querying post data: " + e);
+//                }
+//            }
+//        });
+//    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//    private void goToCreateActivity() {
+//        Log.i(TAG, "Starting create activity");
+//        Intent intent = new Intent(FeedActivity.this, CreatePostActivity.class);
+//        startActivityForResult(intent, CREATE_POST_REQUEST_CODE);
+//    }
 
-        if (requestCode == CREATE_POST_REQUEST_CODE && resultCode == RESULT_OK) {
-            // TODO: Refresh the recyclerview to have the newest post
-        } else if (requestCode == USER_SETTINGS_REQUEST_CODE && resultCode == RESULT_OK) {
-            // This result implies they hit the logout button
-            backToLogin();
-        }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//
+//        if (requestCode == CREATE_POST_REQUEST_CODE && resultCode == RESULT_OK) {
+//            // TODO: Refresh the recyclerview to have the newest post
+//        } else if (requestCode == USER_SETTINGS_REQUEST_CODE && resultCode == RESULT_OK) {
+//            // This result implies they hit the logout button
+//            backToLogin();
+//        }
+//
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
 
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void backToLogin() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if (item.getItemId() == R.id.menuBtnUserDetails) {
-            // Go to create user details
-            Log.i(TAG, "Entering user details activity");
-            Intent intent = new Intent(this, UserDetails.class);
-            startActivityForResult(intent, USER_SETTINGS_REQUEST_CODE);
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    private void backToLogin() {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//
+//        if (item.getItemId() == R.id.menuBtnUserDetails) {
+//            // Go to create user details
+//            Log.i(TAG, "Entering user details activity");
+//            Intent intent = new Intent(this, UserDetails.class);
+//            startActivityForResult(intent, USER_SETTINGS_REQUEST_CODE);
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
 
